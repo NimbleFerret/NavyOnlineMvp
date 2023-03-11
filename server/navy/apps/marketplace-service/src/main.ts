4a3e84d6d42ac990168858ace32536492899ba24
NimbleFerret <andrew@navy.online>
2023-03-11 18:34:53 +0300

    swagger for marketplace service;

--- server/navy/apps/marketplace-service/src/main.ts
+++ server/navy/apps/marketplace-service/src/main.ts
@@ -1,11 +1,21 @@
 import { Config } from '@app/shared-library/config';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Marketplace')
+    .setDescription('Navy.online marketplace api')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(Config.MARKETPLACE_SERVICE_PORT);
   Logger.log(`Marketplace-Service started at port: ${Config.MARKETPLACE_SERVICE_PORT}`);
 }
