99c1c818151f7da4aeb27f534fde3c53cd7e3f03
NimbleFerret <andrew@navy.online>
2023-04-28 00:21:11 +0300

    qfix;

--- server/navy/libs/shared-library/src/utils.ts
+++ server/navy/libs/shared-library/src/utils.ts
@@ -1,5 +1,14 @@
 export class Utils {
 
+    public static readonly ERROR_EMAIL_EXISTS = 'EMAIL_EXISTS';
+    public static readonly ERROR_EMAIL_NOT_FOUND = 'EMAIL_NOT_FOUND';
+    public static readonly ERROR_WALLET_NOT_FOUND = 'WALLET_NOT_FOUND';
+    public static readonly ERROR_WALLET_EXISTS = 'WALLET_EXISTS';
+    public static readonly ERROR_BAD_SIGNATURE = 'BAD_SIGNATURE';
+    public static readonly ERROR_BAD_AUTH = 'BAD_AUTH';
+    public static readonly ERROR_BAD_PARAMS = 'BAD_PARAMS';
+    public static readonly ERROR_BAD_EMAIL_OR_PASSWORD = 'BAD_EMAIL_OR_PASSWORD';
+
     public static DeleteKeysFromMap(map: Map<String, any>, keys: String[]) {
         keys.forEach(key => {
             map.delete(key);
