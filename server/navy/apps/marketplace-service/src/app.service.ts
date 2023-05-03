a7b39f177d75ee91065174cb9093fc44ef69047f
NimbleFerret <andrew@navy.online>
2023-05-04 01:20:03 +0300

    qfix;

--- server/navy/apps/marketplace-service/src/app.service.ts
+++ server/navy/apps/marketplace-service/src/app.service.ts
@@ -7,7 +7,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Faq, FaqDocument } from '@app/shared-library/schemas/marketplace/schema.faq';
 import { FixtureLoader } from './app.fixture.loader';
-import { Favourite, FavouriteDocument } from '@app/shared-library/schemas/marketplace/schema.favourite';
 
 @Injectable()
 export class AppService implements OnModuleInit {
@@ -19,12 +18,11 @@ export class AppService implements OnModuleInit {
     @InjectModel(Mint.name) private mintModel: Model<MintDocument>,
     @InjectModel(Collection.name) private collectionModel: Model<CollectionDocument>,
     @InjectModel(CollectionItem.name) private collectionItemModel: Model<CollectionItemDocument>,
-    @InjectModel(Faq.name) private faqModel: Model<FaqDocument>,
-    @InjectModel(Favourite.name) private favouriteModel: Model<FavouriteDocument>) {
+    @InjectModel(Faq.name) private faqModel: Model<FaqDocument>) {
   }
 
   async onModuleInit() {
-    const fixtureLoader = new FixtureLoader(this.projectModel, this.collectionModel, this.mintModel, this.collectionItemModel, this.faqModel, this.favouriteModel);
+    const fixtureLoader = new FixtureLoader(this.projectModel, this.collectionModel, this.mintModel, this.collectionItemModel, this.faqModel);
     await fixtureLoader.loadFixtures();
   }
 
